f185c7e4e7f4a2dec45ef6b5043bdec11efccb76
yozaira@gmail.com <yozaira@gmail.com>
2014-06-10 13:58:07 -0400

    Adjusted indentation.

--- article_list.php
+++ article_list.php
@@ -10,22 +10,18 @@ $posts = new BlogPost ();
 $article_cursor = $posts->getAllPosts();  
 // Include the directory where images are stored.
 $img_dir = './admin/images/';
-
 $currentPage = (isset($_GET['page'])) ? (int) $_GET['page'] : 1;
-
 $articlesPerPage = 4;
-
 $skip = ($currentPage - 1) * $articlesPerPage;
-
 $totalArticles = $article_cursor->count();
-
 $totalPages = (int)  ceil($totalArticles / $articlesPerPage);
-
 $article_cursor->skip($skip)->limit($articlesPerPage);  
 ?>
+
     <div class="container"> <!-- stars ther and ends on footer.php file -->
       <div class="row">
          <div class="col-lg-8">  
+
           <!-- 
           Call the getNext() method on it to get the first (in this case,  the only) document in the query result.  Finally, display the title and 
           content of the retrieved document using HTML markup. 
@@ -34,36 +30,45 @@ $article_cursor->skip($skip)->limit($articlesPerPage);
           and then advances the cursor. The documents do not get loaded into memory unless we invoke either getNext() or hasNext() on the cursor, 
           this is good for keeping the memory usage low.    
           -->
+
           <?php while ($article_cursor->hasNext()):  $article = $article_cursor->getNext(); ?>
             <!-- Blog Ttile -->
-          <h1 class="blog-post-title"><a href="article.php?id=<?php echo  urlencode( $article['_id'] );  ?>"><?php  echo $article['title']; ?></a></h1>
-          <!-- Author -->
-          <p class="lead">by <a href="author_public_profile.php?id=<?php if(isset($article['author_id'] ) ) { echo  urlencode( $article['author_id'] ); } ?>">
-          <?php if (!empty($article['author_name'])) {echo $article['author_name']; }?></a>  </p>
-          <!-- Date -->      
-          <p> <span class="glyphicon glyphicon-time"></span><?php if (isset( $article['saved_at']) ) { echo date(' F j, Y g:i a ',  $article['saved_at']->sec ); } ?></p>       
-          <!-- Image -->
-          <img src="<?php  echo (isset($article['img_name'] ) ) ? $img_dir.$article['img_name'] : null; ?>" class="img-responsive">
-          <!-- Blog Content -->
-          <p><?php echo substr($article['content'], 0, 200).'...';                    // display its title and first 200 characters of its content:?></p>
-          <a class="btn btn-primary" href="article.php?id=<?php echo urlencode($article['_id'] );       // this link takes us to the article.php file ?>">
-             Read more<span class="glyphicon glyphicon-chevron-right"></span>
-          </a>
-          <hr>
-          <!--When we click on the Read more link, it takes us to the blog.php file . This file receives the _id of the article as an HTTP GET parameter.
-          We invoke the findOne() method on the articles collection, sending the _id value as a parameter to the method. The findOne() method is used to 
-          retrieve a single document, unlike find()  which we use to retrieve a cursor of a set of documents that we can iterate over: -->          
+            <h1 class="blog-post-title"><a href="article.php?id=<?php echo  urlencode( $article['_id'] );  ?>"><?php  echo $article['title']; ?></a></h1>
+           
+            <!-- Author -->
+            <p class="lead">by <a href="author_public_profile.php?id=<?php if(isset($article['author_id'] ) ) { echo  urlencode( $article['author_id'] ); } ?>">
+            <?php if (!empty($article['author_name'])) {echo $article['author_name']; }?></a>  </p>
+            
+            <!-- Date -->      
+            <p> <span class="glyphicon glyphicon-time"></span><?php if (isset( $article['saved_at']) ) { echo date(' F j, Y g:i a ',  $article['saved_at']->sec ); } ?></p>       
+           
+            <!-- Image -->
+            <img src="<?php  echo (isset($article['img_name'] ) ) ? $img_dir.$article['img_name'] : null; ?>" class="img-responsive">
+           
+            <!-- Blog Content -->
+            <p><?php echo substr($article['content'], 0, 200).'...';                    // display its title and first 200 characters of its content:?></p>
+            <a class="btn btn-primary" href="article.php?id=<?php echo urlencode($article['_id'] );       // this link takes us to the article.php file ?>">
+               Read more<span class="glyphicon glyphicon-chevron-right"></span>
+            </a>
+            <hr>
+
+            <!--When we click on the Read more link, it takes us to the blog.php file . This file receives the _id of the article as an HTTP GET parameter.
+            We invoke the findOne() method on the articles collection, sending the _id value as a parameter to the method. The findOne() method is used to 
+            retrieve a single document, unlike find()  which we use to retrieve a cursor of a set of documents that we can iterate over: -->          
           <?php endwhile; ?>
         
-          <!-- Pagination  -->    
+          <!-- Pagination  -->  
+
           <ul class="pager">
+
           <li class="previous">
-          <?php if ($currentPage !== 1): ?> <a href="<?php echo $_SERVER['PHP_SELF'].'?page='.($currentPage - 1); ?>">Previous </a>
+            <?php if ($currentPage !== 1): ?> <a href="<?php echo $_SERVER['PHP_SELF'].'?page='.($currentPage - 1); ?>">Previous </a>
           </li> <?php endif; ?>
           </ul>
+
           <ul class="pager">
           <li class="previous">
-          <?php if ($currentPage !== $totalPages): ?> <a href="<?php echo $_SERVER['PHP_SELF'].'?page='.($currentPage + 1); ?>">Next</a>
+            <?php if ($currentPage !== $totalPages): ?> <a href="<?php echo $_SERVER['PHP_SELF'].'?page='.($currentPage + 1); ?>">Next</a>
           </li><?php endif; ?>
           </ul>
       
